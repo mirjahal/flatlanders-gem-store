0d2bd534228b62fd3c65d71443de9d3cb289bbbd
Almir Jr <mirjahal@hotmail.com>
2015-10-05 01:14:45 +0000

    Refactoring into a module

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,5 +1,5 @@
 (function() {
-    var app = angular.module('gemStore', []);
+    var app = angular.module('gemStore', ['store-directives']);
 
     app.controller('StoreController', function() {
         this.products = gems; 
@@ -12,61 +12,6 @@
             this.review.createdOn = Date.now();
         	product.reviews.push(this.review);
             this.review = {};
-        };
-    });
-    
-    app.directive('productDescription', function() {
-        return {
-            restrict: 'E',
-            templateUrl: 'views/product-description.html'
-        };
-    });
-    
-    app.directive('productSpecs', function() {
-        return {
-            restrict: 'A',
-            templateUrl: 'views/product-specs.html'
-        };
-    });
-    
-    app.directive('productReviews', function() {
-        return {
-            restrict: 'E',
-            templateUrl: 'views/product-reviews.html'
-        };
-    });
-    
-    app.directive('productTabs', function() {
-        return {
-            restrict: 'E',
-            templateUrl: 'views/product-tabs.html',
-            controller: function() {
-                this.tab = 1;
-                    
-                this.setTab = function(tab) {
-                    this.tab = tab;
-                };
-                    
-                this.isSet = function(selectedTab) {
-                    return this.tab === selectedTab;
-                };
-            },
-            controllerAs: 'tab'
-        };
-    });
-    
-    app.directive('productGallery', function() {
-        return {
-            restrict: 'E',
-            templateUrl: 'views/product-gallery.html',
-            controller: function() {
-                this.current = 0;
-                    
-                this.setCurrent = function(current) {
-                    this.current = (!current) ? 0 : current;
-                };
-            },
-            controllerAs: 'gallery'
         };
     });
     
