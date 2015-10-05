2b4e7fb2228d99ced9d7d060a37d9afbaca6b9c0
Almir Jr <mirjahal@hotmail.com>
2015-10-05 00:05:07 +0000

    Directives

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -35,6 +35,13 @@
         };
     });
     
+    app.directive('productDescription', function() {
+        return {
+            restrict: 'E',
+            templateUrl: 'views/product-description.html'
+        };
+    });
+    
     var gems = [{
         name: 'Azurite',
         description: "Some gems have hidden qualities beyond their luster, beyond their shine... Azurite is one of those gems.",
